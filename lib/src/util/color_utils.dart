af57c361c08cd3830050fada36b081d0b47bd3db
khamidjon.khamidov <khamidjon.khamidov@ridango.com>
2024-03-04 08:52:16 +0200

    return null instead of default color

--- lib/src/util/color_utils.dart
+++ lib/src/util/color_utils.dart
@@ -10,15 +10,14 @@ extension ColorsExtensions on Color {
 
 /// Extension to convert a [String] to a Color
 extension StringExtensions on String {
-  Color toColor() {
+  Color? toColor() {
     try {
       var hexColor = toUpperCase().replaceAll('#', '');
       if (hexColor.length == 6) hexColor = 'FF$hexColor';
       var data = int.parse(hexColor, radix: 16);
       return Color(data);
     } catch (e) {
-      /// Return transparent color if the color is not valid
-      return const Color(0x00000000);
+      return null;
     }
   }
 }
